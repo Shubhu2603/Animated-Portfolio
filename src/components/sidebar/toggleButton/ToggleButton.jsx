22269c0c28861e1035d9c31a408a95f9f027e0c9
Shubhankar Hingne <hingneshubhankar@gmail.com>
2024-07-06 19:01:45 -0700

    fixed import

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {motion} from "framer-motion"
-import "./toggleButton.scss"
+import "./togglebutton.scss"
 
 export default function ToggleButton({setOpen}) {
   return (
